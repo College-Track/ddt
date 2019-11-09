7a258fc0ccc5e97cd781c7ceda022238531291fb
Baker Renneckar <bakerrenneckar@gmail.com>
2019-11-08 16:41:26 -0800

    add json for regions

--- src/helpers/variables.py
+++ src/helpers/variables.py
@@ -2,13 +2,60 @@ SITES = [
     {"sf_name": "College Track at The Durant Center", "short_name": "PGC"},
     {"sf_name": "College Track Ward 8", "short_name": "WARD 8"},
     {"sf_name": "College Track Watts", "short_name": "WATTS"},
-    {"sf_name": "College Track Boyle Height ", "short_name": "BH"},
-    {"sf_name": "College Track New Orleans ", "short_name": "NOLA"},
+    {"sf_name": "College Track Boyle Heights", "short_name": "BH"},
+    {"sf_name": "College Track New Orleans", "short_name": "NOLA"},
     {"sf_name": "College Track Denver", "short_name": "DEN"},
-    {"sf_name": "College Track Auroa", "short_name": "AUR"},
+    {"sf_name": "College Track Aurora", "short_name": "AUR"},
     {"sf_name": "College Track Oakland", "short_name": "OAK"},
     {"sf_name": "College Track Sacramento", "short_name": "SAC"},
     {"sf_name": "College Track San Francisco", "short_name": "SF"},
     {"sf_name": "College Track East Palo Alto", "short_name": "EPA"},
 ]
 
+REGIONS = [
+    {
+        "region_long": "New Orleans",
+        "region_short": "NOLA",
+        "sites_long": ["College Track New Orleans"],
+        "sites_short": ["NOLA"],
+    },
+    {
+        "region_long": "East Coast",
+        "region_short": "East Coast",
+        "sites_long": ["College Track at the Durant Center", "College Track Ward 8"],
+        "sites_short": ["PGC"],
+    },
+    {
+        "region_long": "Bay Area",
+        "region_short": "Bay Area",
+        "sites_long": [
+            "College Track Oakland",
+            "College Track San Francisco",
+            "College Track East Palo Alto",
+        ],
+        "sites_short": ["OAK", "SF", "EPA"],
+    },
+    {
+        "region_long": "Northern California",
+        "region_short": "Nor Cal",
+        "sites_long": [
+            "College Track Oakland",
+            "College Track San Francisco",
+            "College Track East Palo Alto",
+            "College Track Sacramento",
+        ],
+        "sites_short": ["OAK", "SF", "EPA", "SAC"],
+    },
+    {
+        "region_long": "Los Angeles",
+        "region_short": "LA",
+        "sites_long": ["College Track Watts", "College Track Boyle Heights"],
+        "sites_short": ["WATTS", "BH"],
+    },
+    {
+        "region_long": "Colorado",
+        "region_short": "CO",
+        "sites_long": ["College Track Aurora", "College Track Denver"],
+        "sites_short": ["AUR", "DEN"],
+    },
+]

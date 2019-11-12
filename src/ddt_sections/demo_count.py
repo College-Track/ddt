8110bb5a4f72c31e131463aa00d98ac83aa9b453
Baker Renneckar <bakerrenneckar@gmail.com>
2019-11-12 15:55:36 -0800

    Added totals by region to tables

--- src/ddt_sections/demo_count.py
+++ src/ddt_sections/demo_count.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from pathlib import Path
-from src.helpers.variables import REGIONS
+from src.helpers.helpers import generate_sites, generate_regions
+
+SITES = generate_sites()
+REGIONS = generate_regions(SITES)
 
 
 interim_data = Path(".") / "data" / "interim" / "count_demographics_interim.pkl"
@@ -17,18 +20,34 @@ def create_pivot_table(df, type):
         values="18 Digit ID",
         aggfunc="count",
         fill_value=0,
+        margins=True,
     )
     return pivot_table
 
-def calculate_region_totals(df, REGIONS)
 
-first_gen_pivot = create_pivot_table(df, 'Indicator: First Generation')
-gender_pivot = create_pivot_table(df, 'Gender')
-race_pivot = create_pivot_table(df, 'Ethnic background')
+# TODO: Determine how to manage sites that aren't in dataframe
+def calculate_region_totals(df, REGIONS):
+    for region in REGIONS:
+        df[region.region_short] = 0
+        _sites = [site.short_name for site in region.sites]
+        df[region.region_short] = df[_sites].sum(axis=1)
+
+    return df
 
 
-print(first_gen_pivot)
+first_gen_pivot = create_pivot_table(df, "Indicator: First Generation")
+gender_pivot = create_pivot_table(df, "Gender")
+race_pivot = create_pivot_table(df, "Ethnic background")
 
-print(gender_pivot)
 
-print(race_pivot)
+first_gen_pivot = calculate_region_totals(first_gen_pivot, REGIONS)
+gender_pivot = calculate_region_totals(gender_pivot, REGIONS)
+race_pivot = calculate_region_totals(race_pivot, REGIONS)
+
+
+# print(first_gen_pivot)
+
+# print(gender_pivot)
+
+# print(race_pivot)
+

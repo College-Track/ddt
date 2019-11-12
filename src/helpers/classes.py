dc74c130a611b1f3d337cbe04410297328751e73
Baker Renneckar <bakerrenneckar@gmail.com>
2019-11-12 15:56:11 -0800

    fixed bug in region class for sites

--- src/helpers/classes.py
+++ src/helpers/classes.py
@@ -1,27 +1,34 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List
 
 
-class Site():
+class Site:
     site_instances = {}
+
     def __init__(self, sf_name, short_name):
         self.sf_name = sf_name
         self.short_name = short_name
-        Site.site_instances[self.short_name] = self 
+        Site.site_instances[self.short_name] = self
 
     def __repr__(self):
-        return (f'{self.__class__.__name__}'
-        f'(sf_name={self.sf_name!r}, short_name={self.short_name!r})')
-    
+        return (
+            f"{self.__class__.__name__}"
+            f"(sf_name={self.sf_name!r}, short_name={self.short_name!r})"
+        )
 
 
-@dataclass()
 class Region:
-    __slots__ = ["region_long", "region_short"]
-    region_long: str
-    region_short: str
-    sites = []
+    region_instances = []
 
+    def __init__(self, region_long, region_short):
+        self.region_long = region_long
+        self.region_short = region_short
+        self.sites = []
+        Region.region_instances.append(self)
 
-
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}"
+            f"(region_long={self.region_long!r}, region_short={self.region_short!r})"
+        )
 
--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -2,9 +2,13 @@ from src.helpers.classes import Site, Region
 
 
 def generate_sites():
-
+    """
+    Used to generate a dictionary of all the active sites.
+    If a new site is created, it must be added to this function
+    The dictionary is instantiated in the Site class
+    """
     _pgc = Site("College Track at The Durant Center", "PGC")
-    _ward_8 = Site("College Track Ward 8", "WARD 8")
+    # _ward_8 = Site("College Track Ward 8", "WARD 8")
     _watts = Site("College Track Watts", "WATTS")
     _boyle_heights = Site("College Track Boyle Heights", "BH")
     _nola = Site("College Track New Orleans", "NOLA")
@@ -19,7 +23,11 @@ def generate_sites():
 
 
 def generate_regions(SITES):
-    REGIONS = []
+    """
+    Used to generate a list of all regions and the sites
+    that belong in the region.
+    The list lives on the Region class.
+    """
     _nor_cal = Region("Northern California", "Nor Cal")
     _nola_region = Region("New Orleans", "NOLA")
     _east_coast = Region("East Coast", "East Coast")
@@ -30,24 +38,13 @@ def generate_regions(SITES):
 
     _nor_cal.sites.extend([SITES["EPA"], SITES["SF"], SITES["OAK"], SITES["SAC"]])
     _nola_region.sites.extend([SITES["NOLA"]])
-    _east_coast.sites.extend([SITES["WARD 8"], SITES["PGC"]])
+
+    _east_coast.sites.extend([SITES["PGC"]])
     _bay_area.sites.extend([SITES["EPA"], SITES["SF"], SITES["OAK"]])
     _la_region.sites.extend([SITES["WATTS"], SITES["BH"]])
     _colorado.sites.extend([SITES["AUR"], SITES["DEN"]])
 
-    for site in SITES.keys():
+    for site in SITES.values():
+        _national.sites.append(site)
 
-        _national.sites.append(SITES[site])
-
-    REGIONS.extend(
-        [
-            _nor_cal,
-            _nola_region,
-            _east_coast,
-            _bay_area,
-            _la_region,
-            _colorado,
-            _national,
-        ]
-    )
-    return REGIONS
+    return Region.region_instances

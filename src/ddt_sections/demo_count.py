c614a1d0f3a25bba4e02012028d2e41133f0e75d
Baker Renneckar <bakerrenneckar@gmail.com>
2019-11-19 11:43:45 -0800

    added income pivot

--- src/ddt_sections/demo_count.py
+++ src/ddt_sections/demo_count.py
@@ -25,7 +25,7 @@ def create_pivot_table(df, type):
         fill_value=0,
         margins=True,
     )
-    pivot_table = pivot_table.drop('All', axis=1)
+    pivot_table = pivot_table.drop("All", axis=1)
     return pivot_table
 
 
@@ -41,26 +41,30 @@ def calculate_region_totals(df, REGIONS):
 
     return df
 
-# TODO: Currently is in active, need to have a better solution
+
+# TODO: Currently is inactive, need to have a better solution
 def add_subtotals(df):
     tmp_df = df.sum(level=1, axis=0)
     tmp_df.name = "Totals"
-    
+
     return df.append(tmp_df)
+
 
 first_gen_pivot = create_pivot_table(df, "Indicator: First Generation")
 gender_pivot = create_pivot_table(df, "Gender")
 race_pivot = create_pivot_table(df, "Ethnic background")
+income_pivot = create_pivot_table(df, "Indicator: Low-Income")
 
 
 first_gen_pivot = calculate_region_totals(first_gen_pivot, REGIONS)
 gender_pivot = calculate_region_totals(gender_pivot, REGIONS)
 race_pivot = calculate_region_totals(race_pivot, REGIONS)
+income_pivot = calculate_region_totals(income_pivot, REGIONS)
 
 first_gen_totals = first_gen_pivot.sum(level=1, axis=0)
 gender_totals = gender_pivot.sum(level=1, axis=0)
 race_totals = race_pivot.sum(level=1, axis=0)
-
+income_totals = income_pivot.sum(level=1, axis=0)
 
 
 # print(first_gen_pivot)

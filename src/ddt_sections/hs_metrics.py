c596da823db76559aa9400e805edb01b98688151
Baker Renneckar <bakerrenneckar@gmail.com>
2019-11-21 12:21:24 -0800

    added student life secion

--- src/ddt_sections/hs_metrics.py
+++ src/ddt_sections/hs_metrics.py
@@ -12,9 +12,6 @@ import numpy as np
 SITES = generate_sites()
 REGIONS = generate_regions(SITES)
 
-# instatiate the excel sheet for all of these metrics
-hs_metrics = Sheet("HS Metrics")
-
 
 def generate_total_workshop(df, workshop_attendance):
 
@@ -86,7 +83,61 @@ def create_workshop_attendance_cat(hs_metrics):
 
     return workshop_attendance
 
+def create_community_service_cat(hs_metrics):
+    student_life = MetricCategory(
+        hs_metrics,
+        "Student Life",
+        "Class of 2019-2022",
+        "Aug 2019",
+        "June 2020"
+    )
 
-workshop_attendance = create_workshop_attendance_cat(hs_metrics)
+    student_life_interim_data = (
+        Path(".") / "data" / "interim" / "community_service_all_time_interim.pkl"
+    )
+
+    student_life_df = pd.read_pickle(student_life_interim_data)
+
+    community_service_cumulative = generate_community_service_cumulative(
+        student_life_df, student_life
+    )
+
+    # community_service_prev_year = generate_community_service_prev_year(
+    #     student_life_df, student_life)
+    
 
 
+def generate_community_service_cumulative(df, student_life):
+    community_service_cumulative = Metric(
+        student_life,
+        "Community Service Hours: Cumulative",
+        )
+    
+    community_service_cumulative_table = pd.pivot_table(
+        df,
+        columns="site_short",
+        values="Hours of Service Completed",
+        aggfunc=np.sum,
+        fill_value=0,
+        margins=True,
+    )
+    
+
+    community_service_cumulative_table = calculate_region_totals(community_service_cumulative_table, REGIONS)
+    community_service_cumulative.df = community_service_cumulative_table
+
+    return community_service_cumulative
+    
+
+def create_hs_metric_sheet():
+    # instatiate the excel sheet for all of these metrics
+    hs_metrics = Sheet("HS Metrics")
+    workshop_attendance = create_workshop_attendance_cat(hs_metrics)
+    student_life = create_community_service_cat(hs_metrics)
+
+    return hs_metrics
+
+
+hs_metrics = create_hs_metric_sheet()
+
+print(hs_metrics.metric_category)

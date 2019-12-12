15f0fc0e8f28759a0896ef4195cc2fbaabdb53cd
Baker Renneckar <bakerrenneckar@gmail.com>
2019-12-12 12:02:48 -0800

    filtering data so only 5 students from each site appear

--- src/data/scrub_data.py
+++ src/data/scrub_data.py
@@ -12,17 +12,17 @@ from datetime import datetime, timedelta
 raw_data = Path(".") / "data" / "raw"
 
 
-df = pd.read_csv(raw_data.joinpath("student_count_demo.csv"), parse_dates=['Birthdate'])
+df = pd.read_csv(raw_data.joinpath("student_count_demo.csv"))
 
 # Converting Birthdate to datetime object in order to randomize it
 
-df['Birthdate'] = pd.to_datetime(df.Birthdate, errors='coerce')
+df["Birthdate"] = pd.to_datetime(df.Birthdate, errors="coerce")
 
-# Randomizing the birthdate by adding or subtracting 30 days 
-df['Birthdate'] = df.Birthdate.apply(lambda x: x + timedelta(days=randint(-30, 30)))
+# Randomizing the birthdate by adding or subtracting 30 days
+df["Birthdate"] = df.Birthdate.apply(lambda x: x + timedelta(days=randint(-30, 30)))
 
 # Converting back into the original string format
-df['Birthdate'] = df.Birthdate.dt.strftime("%m/%d/%Y")
+df["Birthdate"] = df.Birthdate.dt.strftime("%m/%d/%Y")
 
 # Replacing names with a split up version of 18 Digit ID
 df["Full Name"] = df["18 Digit ID"].apply(
@@ -30,5 +30,8 @@ df["Full Name"] = df["18 Digit ID"].apply(
 )
 
 
-df.to_csv("test/static_data/test_demo.csv")
+# Filtering the data so only 5 students from each site appear
+df = df.groupby("Site").head(5)
+
+df.to_csv("test/static_data/test_demo.csv", index=False)
 
